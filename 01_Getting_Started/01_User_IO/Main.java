8ea3d25b744c34c918f79e7aa4a8eef28fd42809
siddharth <siddharthnmishra7@gmail.com>
2025-06-03 18:19:21 +0530

    user i/o

--- 01_Getting_Started/01_User_IO/Main.java
+++ 01_Getting_Started/01_User_IO/Main.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /*
 Scanner is easiest to implement, but the slowest
@@ -15,23 +19,46 @@ BufferedReader is faster but requires manual parsing
 
     System.out.println(a + b);
 
-🧠 Do not close readers in cp, you will also close system.in and won't be able to read again, it's fine to keep it open
+🧠 Do not close readers in contests, you will also close system.in and won't be able to read again, it's fine to keep it open
  */
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Enter your name : ");
+        System.out.print("Enter your name : ");
         String name = scanner.nextLine();
 
-        System.out.printf("Enter your age : ");
+        System.out.print("Enter your age : ");
         int age = scanner.nextInt();
 
-        scanner.close();
-        
         System.out.println("Hello " + name + "!");
         System.out.println("You are " + age + " years old.");
+
+        // -------------------------------------------------------------------------
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter the number of integers: ");
+        //reads a single token
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.print("Enter the integers : ");
+        // reads entire line and then breaks down at spaces using StringTokenizer
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            // we have only read the line, we need to parse it as integers too.
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // standard procedure
+        System.out.print("You entered: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        scanner.close();
     }
 }
